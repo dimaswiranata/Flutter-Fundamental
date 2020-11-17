8576529569220cf10a95f49850d3229b40f8214a
dimaswiranata <dimaswiranata8@gmail.com>
2020-11-17 21:58:43 +0700

    belajar flutter html

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:my_first_flutter/course/Belajar_Grafik/time_and_chart.dart';
+import 'package:my_first_flutter/course/Belajar_Flutter_html/belajar_flutter_html.dart';
+// import 'package:my_first_flutter/course/Belajar_Grafik/time_and_chart.dart';
 void main() async {
   // BlocSupervisor.delegate = await HydratedBlocDelegate.build();
   runApp(MyApp());
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: TimeAndChart(),
+      home: BelajarFlutterHtml(),
     );
   }
 }
